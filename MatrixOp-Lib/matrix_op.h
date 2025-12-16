7d5b4dc23b772351995019ae9a9ee00c7e505961
Xian070711 <yinmin5plus@gmail.com>
2025-12-16 15:20:14 +0800

    change

--- MatrixOp-Lib/matrix_op.h
+++ MatrixOp-Lib/matrix_op.h
@@ -3,19 +3,19 @@
 
 #define SIZE 3
 
-/* Basic */
-void mat_add(int A[SIZE][SIZE], int B[SIZE][SIZE], int C[SIZE][SIZE]);
-void mat_sub(int A[SIZE][SIZE], int B[SIZE][SIZE], int C[SIZE][SIZE]);
-void mat_elem_mul(int A[SIZE][SIZE], int B[SIZE][SIZE], int C[SIZE][SIZE]);
+/* Basic operations */
+void mat_add(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
+void mat_sub(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
+void mat_elem_mul(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
 
-/* Linear */
-void mat_mul(int A[SIZE][SIZE], int B[SIZE][SIZE], int C[SIZE][SIZE]);
-void mat_transpose(int A[SIZE][SIZE], int T[SIZE][SIZE]);
+/* Linear operations */
+void mat_mul(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
+void mat_transpose(int A[SIZE][SIZE], int R[SIZE][SIZE]);
 
 /* Advanced */
 int mat_det(int A[SIZE][SIZE]);
-void mat_adjoint(int A[SIZE][SIZE], int adj[SIZE][SIZE]);
-int mat_inverse(int A[SIZE][SIZE], float inv[SIZE][SIZE]);
+
+/* Inverse: compute adj(A), return det(A) */
+int mat_inverse(int A[SIZE][SIZE], int adj[SIZE][SIZE]);
 
 #endif
-
